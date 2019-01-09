eb00fd8729332511d96f9de856ff7d041ba3f2f9
Stella Zhang <stellazhang@stella-zhangs-macbook-2.local>
2019-01-09 11:40:23 -0800

    Create class for Grub and Bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,14 @@
-var Bee = function() {
+var Bee = function(age, color, food, eat, job) {
+  
+  Grub.call(this, food, eat);
+
+  this.age = 5
+  this.color = 'yellow'
+  this.job = 'keep on growing'; 
+
+
 };
+
+Bee.prototype = Object.create(Grub.prototype); //set inheritance
+Bee.prototype.constructor = Bee; // set Bee constructor function
 
--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,15 @@
-var Grub = function() {
+var Grub = function(age, color, food, eat, job) {
+
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
+  
+
 };
 
+Grub.prototype.eat = function() {
+
+};
+
+
+//var grubOne = new Grub(0, 'pink', 'jelly')
